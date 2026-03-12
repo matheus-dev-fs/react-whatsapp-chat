e917f7706b3ffd89c72c42b2e32fb0f045d78687
matheus-dev-fs <matheusmks10@gmail.com>
2026-03-12 18:39:45 -0300

    feat: add styled search area component
    - created Search component with input field and search icon
    - applied visual styling using styled-components for search area and input
    - integrated Search component into the Sidebar layout
    - component is currently visual only; logic will be added in the future

--- src/components/search/search.component.tsx
+++ src/components/search/search.component.tsx
@@ -1,8 +1,14 @@
 import { JSX } from "react";
 import * as C from "./search.style"
+import SearchIcon from '@mui/icons-material/Search';
 
 export const Search = (): JSX.Element => {
     return (
-        <C.HeaderArea>...</C.HeaderArea>
+        <C.SearchArea>
+            <C.SearchInputArea>
+                <SearchIcon fontSize="small" style={{ color: "#919191" }} />
+                <C.SearchInput type="search" placeholder="Procurar ou começar conversa" />
+            </C.SearchInputArea>
+        </C.SearchArea>
     );
 }
--- src/components/search/search.style.ts
+++ src/components/search/search.style.ts
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const HeaderArea = styled.div``;
+export const SearchArea = styled.div`
+    background-color: #f6f6f6;
+    border-bottom: 1px solid #EEE;
+    padding: 5px 15px;
+`;
+
+export const SearchInputArea = styled.div`
+    background-color: #FFF;
+    height: 40px;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+`;
+
+export const SearchInput = styled.input`
+    flex: 1;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    margin-left: 10px;
+`;
