82d582255966babc18cf7518b85eb6f71dbf868c
matheus-dev-fs <matheusmks10@gmail.com>
2026-03-13 18:37:52 -0300

    feat: add structure and styling for chat window footer
    - created ChatWindowFooter component with input field and action buttons
    - applied initial styling for footer layout and elements using styled-components
    - included emoji, send, and mic icons for future interactions
    - logic for emoji picker, message sending, and other actions will be implemented later

--- src/components/chat-window/chat-window-footer/chat-window-footer.component.tsx
+++ src/components/chat-window/chat-window-footer/chat-window-footer.component.tsx
@@ -1,10 +1,27 @@
 import { JSX } from "react";
 import * as C from "./chat-window-footer.styles";
+import { Button } from "../../common/Button.style";
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import CloseIcon from '@mui/icons-material/Close';
+import SendIcon from '@mui/icons-material/Send';
+import MicIcon from '@mui/icons-material/Mic';
 
 export const ChatWindowFooter = (): JSX.Element => {
     return (
         <C.ChatWindowFooterArea>
-            
+            <C.ChatWindowFooterPre>
+                <Button>
+                    <InsertEmoticonIcon style={{ color: '#919191' }}/>
+                </Button>
+            </C.ChatWindowFooterPre>
+            <C.ChatWindowFooterInputArea>
+                <C.ChatWindowFooterInput type="text" placeholder="Digite uma mensagem"/>
+            </C.ChatWindowFooterInputArea>
+            <C.ChatWindowFooterPos>
+                <Button>
+                    <SendIcon style={{ color: '#919191' }}/>
+                </Button>
+            </C.ChatWindowFooterPos>
         </C.ChatWindowFooterArea>
     )
 }
--- src/components/chat-window/chat-window-footer/chat-window-footer.styles.ts
+++ src/components/chat-window/chat-window-footer/chat-window-footer.styles.ts
@@ -1,3 +1,33 @@
 import styled from 'styled-components';
 
-export const ChatWindowFooterArea = styled.div``;
+export const ChatWindowFooterArea = styled.div`
+    height: 62px;
+    display: flex;
+    align-items: center;
+`;
+
+export const ChatWindowFooterPre = styled.div`
+    display: flex;
+    margin: 0 15px;
+`;
+
+export const ChatWindowFooterInputArea = styled.div`
+    flex: 1;
+`;
+
+export const ChatWindowFooterInput = styled.input`
+    width: 100%;
+    height: 40px;
+    border: 0;
+    outline: 0;
+    background-color: #FFF;
+    border-radius: 20px;
+    font-size: 15px;
+    color: #4A4A44;
+    padding-left: 15px;
+`;
+
+export const ChatWindowFooterPos = styled.div`
+    display: flex;
+    margin: 0 15px;
+`;
